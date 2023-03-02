7687f35aaa708d840b5161cffb9f7f2dc4c32232
Fyl Andrii <fil.andrey91@gmail.com>
2023-03-02 16:38:08 +0200

    ++

--- js/mob_menu.js
+++ js/mob_menu.js
@@ -1,27 +1,18 @@
 
     (() => {
         const openMenuBurger = document.getElementById("burger");
-        const headerMenu = document.querySelector(".header_mob-tab");
+        const svgOpenBurger = document.querySelector(".icon-burger-menu_mob-tab");
+        const heroSection = document.querySelector(".hero__section");
         const closeMenuBtn = document.querySelector(".header_mob-menu_close-btn");
+       
         openMenuBurger.addEventListener("click", () => {
-            headerMenu.classList.add("open");  
+            heroSection.classList.add("is-open"); 
+                svgOpenBurger.style.fill = "#fcb93f";    
         })
+
         closeMenuBtn.addEventListener("click", () => {
-           headerMenu.classList.remove("open"); 
+            heroSection.classList.remove("is-open"); 
+             svgOpenBurger.style.fill = "white"
+            
         })
 })();
-
-
-
-
-//     (() => {
-//         const openMenuBurger = document.getElementById("burger");
-//         const heroSection = document.querySelector(".hero__section");
-//         const closeMenuBtn = document.querySelector(".header_mob-menu_close-btn");
-//         openMenuBurger.addEventListener("click", () => {
-//             heroSection.classList.add("open");  
-//         })
-//         closeMenuBtn.addEventListener("click", () => {
-//            heroSection.classList.remove("open"); 
-//         })
-// })();
