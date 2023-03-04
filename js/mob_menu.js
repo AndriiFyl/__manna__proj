5d29958d6249452b9c05d16ed31e8608eade5ce7
Fyl Andrii <fil.andrey91@gmail.com>
2023-03-04 17:32:20 +0200

    adds adaptive layouts to other sections

--- js/mob_menu.js
+++ js/mob_menu.js
@@ -2,16 +2,16 @@
     (() => {
         const openMenuBurger = document.getElementById("burger");
         const svgOpenBurger = document.querySelector(".icon-burger-menu_mob-tab");
-        const heroSection = document.querySelector(".hero__section");
+        const common_section = document.querySelector(".section_for_menu");
         const closeMenuBtn = document.querySelector(".header_mob-menu_close-btn");
        
         openMenuBurger.addEventListener("click", () => {
-            heroSection.classList.add("is-open"); 
+            common_section.classList.add("is-open"); 
                 svgOpenBurger.style.fill = "#fcb93f";    
         })
 
         closeMenuBtn.addEventListener("click", () => {
-            heroSection.classList.remove("is-open"); 
+            common_section.classList.remove("is-open"); 
              svgOpenBurger.style.fill = "white"
             
         })
